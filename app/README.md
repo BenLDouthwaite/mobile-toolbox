29a5aab6a6172bbb75a568305e4b41570f1e26a4
Ben Douthwaite <Ben.L.Douthwaite@gmail.com>
2022-05-30 21:42:37 +0100

    Adding landing page simple menu, links out to separate activities, that link up to home again

---
+++ app/README.md
@@ -0,0 +1,11 @@
+# Mobile Toolbox
+
+## Todo list. 
+
+- [ ] Activities linking to each other?
+
+
+### Task list 
+
+- [ ] Data entry from user
+- [ ] Persistence to local filesystem. 
--- app/src/main/java/com/bld/mobileapplication/MainActivity.kt
+++ app/src/main/java/com/bld/mobileapplication/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.bld.mobileapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun loadBirthdayCard(view: View) {
+        println("Load Birthday Card")
+        val birthdayMessage = "Happy Birthday!"
+        val intent = Intent(this, BirthdayCardActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, birthdayMessage)
+        }
+        startActivity(intent)
+    }
+
+    fun loadToDo(view: View) {
+        println("Load To Do")
+        val intent = Intent(this, ToDoActivity::class.java)
+        startActivity(intent)
+    }
+
 }
